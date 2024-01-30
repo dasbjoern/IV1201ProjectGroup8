bbf9797da1990c227251b15abcb3d694813fbc39
Bjorn <bnola@kth.se>
2024-01-30 13:02:01 +0100

    maven with spring

---
+++ iv1201/src/test/java/com/projectgroup8/iv1201/Iv1201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectgroup8.iv1201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iv1201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

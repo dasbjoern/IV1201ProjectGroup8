9aaea08380a357ddafa6fcca06eab2052a164572
Bjorn <bnola@kth.se>
2024-01-30 13:07:25 +0100

    maven with spring

---
+++ src/main/java/com/projectgroup8/iv1201/Iv1201Application.java
@@ -0,0 +1,13 @@
+package com.projectgroup8.iv1201;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iv1201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iv1201Application.class, args);
+	}
+
+}

3761a1fe2c0782b7282f25541c76ef996d3053de
Bjorn <bnola@kth.se>
2024-02-01 14:38:48 +0100

    folder structure

---
+++ src/main/java/com/projectgroup8/iv1201/integration/availability.java
@@ -0,0 +1,6 @@
+package com.projectgroup8.iv1201.persistence;
+@Entity
+@Table(name="availability")
+public class availability {
+    
+}
---
+++ src/main/java/com/projectgroup8/iv1201/integration/person.java
@@ -0,0 +1,6 @@
+package com.projectgroup8.iv1201.integration;
+
+//add DTO
+public class person {
+    
+}

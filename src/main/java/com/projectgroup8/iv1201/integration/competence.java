dbbdededbb0125656a93829d2dd3cedcce4976f9
Bjorn <bnola@kth.se>
2024-02-01 18:51:12 +0100

    added persistence JPA, persistence.xml and a working query for the Competence table

---
+++ src/main/java/com/projectgroup8/iv1201/integration/competence.java
@@ -0,0 +1,46 @@
+package com.projectgroup8.iv1201.integration;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="competence")
+@NamedQuery(name = "Competence.findname", query = "SELECT name FROM Competence")
+public class Competence{
+
+    public Competence(){
+    }
+    public Competence(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="competence_id")
+    private long id;
+    
+    @Column(name="name")
+    private String name;
+    
+    public void setId(long id){
+        this.id = id;
+    }
+    public long getId(){
+        return this.id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+   
+
+}

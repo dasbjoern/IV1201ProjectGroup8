e0aad307cf0cccf3ffa712dacffe83ace3f9894e
Bjorn <bnola@kth.se>
2024-02-16 10:58:58 +0100

    changed folder for htmls

--- src/main/java/com/projectgroup8/iv1201/configuration/ConfigureApp.java
+++ src/main/java/com/projectgroup8/iv1201/configuration/ConfigureApp.java
@@ -29,49 +29,4 @@ import javax.xml.crypto.Data;
 public class ConfigureApp {
     
 
-//  @Bean
-//   public DataSource getDataSource() throws Exception{
-    
-//     DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-//     dataSourceBuilder.url("${DATABASE_URL}");
-//     dataSourceBuilder.username("${DATABASE_USER}");
-//     dataSourceBuilder.password("${DATABASE_PASS}");
-//     return dataSourceBuilder.build();
-    
-//   }
-//     @Bean
-//     public ServletContextTemplateResolver templateResolver() {
-//     ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
-//     templateResolver.setPrefix("/web/views/");
-//     templateResolver.setSuffix(".html");
-
-//     return templateResolver;
-// }
-
-//     @Bean
-//     public SpringTemplateEngine templateEngine() {
-//     SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-//     templateEngine.setTemplateResolver(templateResolver());
-//     templateEngine.setTemplateEngineMessageSource(messageSource());
-//     return templateEngine;
-// }
-
-//     @Bean
-//     public ThymeleafViewResolver viewResolver(){
-//     ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-//     viewResolver.setTemplateEngine(templateEngine());
-//     return viewResolver;
-// }
-
-    // @Bean
-    // public DataSource dataSource(){
-    //     DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-    //     // dataSourceBuilder.driverClassName("org.postgresql.Driver");
-
-    //     dataSourceBuilder.url("jdbc:postgresql://127.0.0.1:5432/test");
-    //     dataSourceBuilder.username("postgres");
-    //     dataSourceBuilder.password("bjorn123");
-
-    //     return dataSourceBuilder.build();
-    // }
 }

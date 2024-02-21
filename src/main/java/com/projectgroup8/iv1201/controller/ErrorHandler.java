649d1db590275df78952358f7125de465debecc7
Bjorn <bnola@kth.se>
2024-02-21 23:13:03 +0100

    Registration error handling + javadoc

--- src/main/java/com/projectgroup8/iv1201/controller/ErrorHandler.java
+++ src/main/java/com/projectgroup8/iv1201/controller/ErrorHandler.java
@@ -9,32 +9,32 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.projectgroup8.iv1201.model.RecruitmentException;
 
 @Controller
 @ControllerAdvice
-// @SessionAttributes({"isLoggedIn", "personId"})
+@SessionAttributes({"isLoggedIn", "personId"})
 public class ErrorHandler {
-    public static final String loginError = "loginErrorMessage";
-    public static final String loginFailed = "Login failed.";
-    // public static final String noUsername = "Please enter a username.";
-    // public static final String noPassword = "Please enter a password.";
+    // public static final String loginError = "loginErrorMessage";
+    // public static final String loginFailed = "Login failed.";
+   
 
-    @GetMapping("/ErrorNoPerson")
-    public String ErrorNoPerson(Model model){
-        model.addAttribute("errorMessage", "Error person cound not be found.");
+    // @GetMapping("/ErrorNoPerson")
+    // public String ErrorNoPerson(Model model){
+    //     model.addAttribute("errorMessage", "Error person cound not be found.");
         
-        return "errorPage";
-    }
+    //     return "errorPage";
+    // }
 
     
     /**
      * Sends an error if no handler is found, page not found.
      * @param model
-     * @param e
-     * @return
+     * @param e error message
+     * @return error message to error page
      */
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
@@ -47,16 +47,30 @@ public class ErrorHandler {
         return "errorPage";
     }
 
+    
+    /**
+     * Handles Illegal arguments during registration that cause internal error
+     * @param model
+     * @param e error message
+     * @return returns error message to error page
+     */
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String registerException(Model model, Exception e){
         model.addAttribute("errorMessage", "Illegal arguments during registration.");
         
         //TEMP should not be in view.
-        model.addAttribute("internalError", e);
+        // model.addAttribute("internalError", e);
 
         return "errorPage";
     }
+    
+    /**
+     * Handles exception 
+     * @param model
+     * @param e
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String myException(Model model, Exception e){
@@ -67,6 +81,13 @@ public class ErrorHandler {
 
         return "errorPage";
     }
+
+    /**
+     * Handles exception for missing cryptation algorithm.
+     * @param model
+     * @param e Error message
+     * @return error to error page
+     */
     @ExceptionHandler(NoSuchAlgorithmException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String passwordSHAException(Model model, Exception e){
@@ -76,6 +97,12 @@ public class ErrorHandler {
         return "errorPage";
     }
     
+    /**
+     * Handles buisness logic exceptions.
+     * @param model
+     * @param exceptionMessage
+     * @return Error to errorpage
+     */
     @ExceptionHandler(RecruitmentException.class)
     @ResponseStatus(HttpStatus.OK)
     public String recruitException(Model model, RecruitmentException exceptionMessage){
@@ -85,6 +112,25 @@ public class ErrorHandler {
         return "errorPage";
     }
 
+    /**
+     * Returns an appropriate error message when database contraints are broken.
+     * @param e Error message with info about the contraint.
+     * @return returns a String with the appropriate error message.
+     */
+    public static String parseRegisterException(Exception e){
+        String error = "Could not create account.";
+        if(e.getMessage().contains("username_unq")){
+            error = "Username already exists.";
+        }
+        if(e.getMessage().contains("pnr_unq")){
+            error = "Person number already exists.";
+        }
+        if(e.getMessage().contains("email_unq")){
+            error = "Email already exists.";
+        }
+
+        return error;
+    }
     
   
 }
--- src/main/java/com/projectgroup8/iv1201/model/LoginForm.java
+++ src/main/java/com/projectgroup8/iv1201/model/LoginForm.java
@@ -1,14 +1,17 @@
 package com.projectgroup8.iv1201.model;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class LoginForm {
     
-    @Size(min = 3, max = 30, message = "Username should be between 3 and 30 characters.")
+    // @NotBlank(message = "Cannot be blank")
+    @Size(min = 3, max = 30, message = " Username should be between 3 and 30 characters.")
     private String username;
 
-    @Size(min = 3, max = 30, message = "Password should be between 3 and 30 characters.")
+    // @NotBlank(message = "Cannot be blank")
+    @Size(min = 3, max = 30, message = " Password should be between 3 and 30 characters.")
     private String password;
 
     public void setUsername(String username){
